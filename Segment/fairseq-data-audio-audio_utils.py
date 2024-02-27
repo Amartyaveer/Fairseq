564077f16b721df757b1977de64414bd06ff30ec
Amartyaveer <amartyaveer72@gmail.com>
2024-02-27 16:46:45 +0530

    updated mmap

--- Segment/fairseq-data-audio-audio_utils.py
+++ Segment/fairseq-data-audio-audio_utils.py
@@ -1,5 +1,5 @@
-'''avupdated the function mmap_read'''
-def mmap_read(path str, offset int, length int) - bytes
+'''updated the function mmap_read'''
+def mmap_read(path: str, offset: int, length: int) -> bytes:
     output_byteio = io.BytesIO()
     audio, sr = soundfile.read(path, start=offset, stop=offset + length)
     soundfile.write(output_byteio, audio, samplerate=sr, format='wav')
